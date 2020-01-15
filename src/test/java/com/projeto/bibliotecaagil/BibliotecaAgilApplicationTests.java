3d3ce4e1bcaca1dd3fd72da63a14f1b19281cd62
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-01-15 11:27:15 -0300

    🎉 Start project

---
+++ src/test/java/com/projeto/bibliotecaagil/BibliotecaAgilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.bibliotecaagil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaAgilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
