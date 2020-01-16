f670656f78292f3bcc877e0b85e752173f335d44
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-01-16 18:51:20 -0300

    ♻️ Refatorar LivroRepository para extender JpaRepository

--- src/main/java/com/projeto/bibliotecaagil/api/models/repositories/LivroRepository.java
+++ src/main/java/com/projeto/bibliotecaagil/api/models/repositories/LivroRepository.java
@@ -1,9 +1,9 @@
 package com.projeto.bibliotecaagil.api.models.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projeto.bibliotecaagil.api.models.entities.Livro;
 
-public interface LivroRepository extends CrudRepository<Livro, Long> {
+public interface LivroRepository extends JpaRepository<Livro, Long> {
 
 }
