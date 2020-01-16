37ecf00a3d498c850e92703aba5b6a2ccf6793b6
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-01-16 18:46:31 -0300

    ✨ Adicionar recurso para criar novo livro

--- src/main/java/com/projeto/bibliotecaagil/api/resources/LivroResource.java
+++ src/main/java/com/projeto/bibliotecaagil/api/resources/LivroResource.java
@@ -1,8 +1,12 @@
 package com.projeto.bibliotecaagil.api.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projeto.bibliotecaagil.api.models.entities.Livro;
@@ -18,5 +22,11 @@ public class LivroResource {
 	@GetMapping
 	public Iterable<Livro> index() {
 		return livroRepository.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Livro store(@RequestBody Livro livro ) {
+		return livroRepository.save(livro);
 	}
 }
