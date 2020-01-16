6749e4a14fb98ec067271cd19c6cce5d37b8ed58
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-01-16 18:43:54 -0300

    🗃 Adcionar coluna status e dados iniciais

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-insert into livros(numero, ano_publicado, autor, titulo) values(null, 2017, 'Alexandro Aolchique', 'Como fazer sentido e bater o martelo'), (null, 2001, 'Tio Bob', 'Código Limpo'), (null, 2010, 'Hortência Marcari', 'Basquete 101');
+insert into livros(numero, ano_publicado, autor, status, titulo) values(null, 2017, 'Alexandro Aolchique', 'DISPONIVEL', 'Como fazer sentido e bater o martelo'), (null, 2001, 'Tio Bob', 'DISPONIVEL', 'Código Limpo'), (null, 2010, 'Hortência Marcari', 'DISPONIVEL', 'Basquete 101');
 
