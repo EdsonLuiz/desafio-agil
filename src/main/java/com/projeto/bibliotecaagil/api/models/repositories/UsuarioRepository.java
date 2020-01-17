025ff6fe8a3b584a80bb33ef0f504370c25021ba
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-01-17 18:09:46 -0300

    ♻️ Adiconar método para buscar usuário por email

--- src/main/java/com/projeto/bibliotecaagil/api/models/repositories/UsuarioRepository.java
+++ src/main/java/com/projeto/bibliotecaagil/api/models/repositories/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package com.projeto.bibliotecaagil.api.models.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projeto.bibliotecaagil.api.models.entities.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+	public Optional<Usuario> findByEmail(String email);
 }
