86c09cf8b1f4b4ddcb796b091fb22cf3a7d61bff
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-01-17 18:15:52 -0300

    ♻️ Usar passwordEncoder para encodar password

--- src/main/java/com/projeto/bibliotecaagil/api/resources/UsuarioResource.java
+++ src/main/java/com/projeto/bibliotecaagil/api/resources/UsuarioResource.java
@@ -1,6 +1,7 @@
 package com.projeto.bibliotecaagil.api.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,12 @@ public class UsuarioResource {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	@PostMapping
 	public Usuario store(@RequestBody Usuario usuario) {
+		usuario.setPassword(this.passwordEncoder.encode(usuario.getPassword()));
 		return usuarioRepository.save(usuario);
 	}
 }
