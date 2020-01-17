1b6ec3591e4c7e110056102f5c669af47a5974fd
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-01-17 13:32:47 -0300

    ✨ Criar entidade Emprestimo

---
+++ src/main/java/com/projeto/bibliotecaagil/api/models/entities/Emprestimo.java
@@ -0,0 +1,106 @@
+package com.projeto.bibliotecaagil.api.models.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "emprestimos")
+public class Emprestimo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@OneToOne
+	private Livro livro;
+	
+	@Column(name = "data_retirada", nullable = false)
+	private LocalDate dataRetirada;
+	
+	@Column(name = "data_entrega", nullable = true)
+	private LocalDate dataEntrega;
+	
+	public Emprestimo() {
+	}
+	
+	
+
+	public Emprestimo(Livro livro) {
+		super();
+		this.livro = livro;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+	
+	@PrePersist
+	public void defaultInitialDate() {
+		this.dataRetirada = LocalDate.now();
+	}
+
+	public LocalDate getDataRetirada() {
+		return dataRetirada;
+	}
+
+	public void setDataRetirada(LocalDate dataRetirada) {
+		this.dataRetirada = dataRetirada;
+	}
+
+	public LocalDate getDataEntrega() {
+		return dataEntrega;
+	}
+
+	public void setDataEntrega(LocalDate dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Emprestimo other = (Emprestimo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
