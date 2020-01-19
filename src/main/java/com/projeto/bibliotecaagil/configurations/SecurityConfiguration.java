1be12479114572e14a47ab8bdd4527873ef199ce
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-01-19 14:05:28 -0300

    ♻️ Configurar o uso de JwtUserAndPasswordAuthenticationFilter em todas as requisições

--- src/main/java/com/projeto/bibliotecaagil/configurations/SecurityConfiguration.java
+++ src/main/java/com/projeto/bibliotecaagil/configurations/SecurityConfiguration.java
@@ -6,8 +6,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.projeto.bibliotecaagil.configurations.jwt.JwtUserAndPasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -28,12 +31,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
+		http
+			.csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.addFilter(new JwtUserAndPasswordAuthenticationFilter(authenticationManager()))
+			.authorizeRequests()
 			.antMatchers(HttpMethod.POST, "/usuarios/**").permitAll()
 			.antMatchers(HttpMethod.GET, "/livros/**").permitAll()
 			.antMatchers("/h2-console/**").permitAll()
 			.anyRequest().authenticated()
-			.and().httpBasic()
 			.and().headers().frameOptions().sameOrigin();
 	}
 }
